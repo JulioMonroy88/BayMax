17476e90d5834c41cd2ce8f6d7dae2263506e237
JulioMonroy88 <juliocastilloooo11@gmail.com>
2026-05-19 11:24:18 -0600

    terminado

---
+++ js/app.js
@@ -0,0 +1,72 @@
+let nextBtn = document.querySelector('.next')
+let prevBtn = document.querySelector('.prev') 
+
+let slider = document.querySelector('.slider')
+let sliderList = slider.querySelector('.slider .list')
+let thumbnail = document.querySelector('.thumbnail')
+let thumbnailItems = thumbnail.querySelectorAll('.item') 
+
+thumbnail.appendChild(thumbnailItems[0]) 
+
+nextBtn.onclick = function() {
+    moveSlider('next')
+} 
+
+prevBtn.onclick = function() {
+    moveSlider('prev')
+} 
+
+function moveSlider(direction) {
+   let sliderItems = sliderList.querySelectorAll('.item')
+   let thumbnailItems = document.querySelectorAll('.thumbnail .item')
+
+   if(direction === 'next'){
+    sliderList.appendChild(sliderItems[0]) 
+    thumbnail.appendChild(thumbnailItems[0]) 
+    slider.classList.add('next')
+   } else {
+    sliderList.prepend(sliderItems[sliderItems.length - 1])
+    thumbnail.prepend(thumbnailItems[thumbnailItems.length - 1]) 
+    slider.classList.add('prev')
+   }
+
+
+  slider.addEventListener('animationend', function() {
+       if(direction === 'next'){
+           slider.classList.remove('next')
+       } else {
+           slider.classList.remove('prev')
+       }
+   }, {once: true})
+}   
+
+
+document.querySelectorAll('.slider .list .item .content .button button')
+  .forEach(boton => {
+    boton.addEventListener('click', function() {
+      const content = this.closest('.content')
+      content.classList.toggle('show')
+
+      // Cambiar texto del botón
+      if (content.classList.contains('show')) {
+        this.textContent = "Ver menos"
+      } else {
+        this.textContent = "Ver más"
+      }
+    })
+  })
+
+
+
+  let lastScrollTop = 0
+  const navbar = document.querySelector("header")
+
+  window.addEventListener("scroll", function () {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop
+    if (scrollTop > lastScrollTop) {
+      navbar.style.top = "-70px"
+    } else {
+      navbar.style.top = "0"
+    }
+    lastScrollTop = scrollTop
+}) 
